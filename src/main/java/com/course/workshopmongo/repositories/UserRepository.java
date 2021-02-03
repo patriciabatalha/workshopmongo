4a0a5de05d4c0ee1655dcf9998af848948973092
Patricia Batalha <patibatalha@gmail.com>
2021-02-03 13:59:29 -0300

    Database instantiation

---
+++ src/main/java/com/course/workshopmongo/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.course.workshopmongo.repositories;
+
+import com.course.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/course/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.course.workshopmongo.services;
+
+import com.course.workshopmongo.domain.User;
+import com.course.workshopmongo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+
+}
