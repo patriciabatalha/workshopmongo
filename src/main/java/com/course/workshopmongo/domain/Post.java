a06b23a5a35b078e3351497e3a6d14f06c69bd4f
Patricia Batalha <patibatalha@gmail.com>
2021-02-03 17:21:58 -0300

    Adding comments to posts

--- src/main/java/com/course/workshopmongo/domain/Post.java
+++ src/main/java/com/course/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.course.workshopmongo.domain;
 
 import com.course.workshopmongo.dto.AuthorDTO;
+import com.course.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Document
 public class Post implements Serializable {
@@ -17,6 +20,8 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post(){
     }
@@ -67,6 +72,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
