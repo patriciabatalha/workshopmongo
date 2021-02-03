75a512a34977882b6f4454ffe9cccda263f5e2fc
Patricia Batalha <patibatalha@gmail.com>
2021-02-03 16:27:16 -0300

    Creating Post entity with User aggregation

---
+++ src/main/java/com/course/workshopmongo/repositories/PostRepository.java
@@ -0,0 +1,10 @@
+package com.course.workshopmongo.repositories;
+
+import com.course.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
