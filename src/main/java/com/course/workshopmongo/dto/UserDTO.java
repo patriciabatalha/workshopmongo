fd48bbb00afca0a3118276ea8413d7e0001dc903
Patricia Batalha <patibatalha@gmail.com>
2021-02-03 14:13:13 -0300

    DTO to return Users

---
+++ src/main/java/com/course/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,46 @@
+package com.course.workshopmongo.dto;
+
+import com.course.workshopmongo.domain.User;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+    private static final long serialVersionUID = 5246193610560604490L;
+
+    private String id;
+    private String name;
+    private String email;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(User obj) {
+        id = obj.getId();
+        name = obj.getName();
+        email = obj.getEmail();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
