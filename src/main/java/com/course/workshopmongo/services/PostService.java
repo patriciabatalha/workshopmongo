6665eb5b264c9f7d6384dd085dc075cfc891bcdc
Patricia Batalha <patibatalha@gmail.com>
2021-02-03 17:40:00 -0300

    Query methods

--- src/main/java/com/course/workshopmongo/services/PostService.java
+++ src/main/java/com/course/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.course.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,4 +20,7 @@ public class PostService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 
+    public List<Post> findByTitle(String text){
+        return repo.findByTitleContainingIgnoreCase(text);
+    }
 }
